4415ba5d58bd72d0ca5227ce78d5b97e836257a2
Giuseppe Cesarano <PecoraInPannaCotta@gmail.com>
2026-01-10 12:24:15 +0100

    user macro for throughput point creation

---
+++ include/pside.h
@@ -0,0 +1,15 @@
+#define PSIDE_THROUGHPUT_POINT(name)                                           \
+  static_assert(sizeof(name) <= 256,                                           \
+                "PSIDE point name exceeds 255 characters");                    \
+  __asm__ __volatile__(                                                        \
+      ".p2align 4\n\t"                                                         \
+      "1:\n\t"                                                                 \
+      ".byte 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00\n\t"               \
+      ".byte 0x0f, 0x1f, 0x40, 0x00\n\t"                                       \
+                                                                               \
+      ".pushsection .pside_throughput, \"R\", @progbits\n\t"                   \
+      ".align 8\n\t"                                                           \
+      ".quad 1b\n\t"                                                           \
+      ".asciz \"" name "\"\n\t"                                                \
+      ".popsection\n\t" ::                                                     \
+          : "rax")
---
+++ include/pside.zig
@@ -0,0 +1,15 @@
+inline fn throughputPoint(comptime name: []const u8) void {
+    if (name.len >= 256) @compileError("PSIDE point name exceeds 255 characters");
+
+    asm volatile (
+        \\   .p2align 4
+        \\ 1:
+        \\   .byte 0x0f, 0x1f, 0x84, 0x00, 0x00, 0x00, 0x00, 0x00
+        \\   .byte 0x0f, 0x1f, 0x40, 0x00
+        \\   .pushsection .pside_throughput, "R", @progbits
+        \\   .align 8
+        \\   .quad 1b
+    ++ "\n" ++ " .asciz \"" ++ name ++ "\"\n" ++
+        \\   .popsection
+    ::: .{ .rax = true });
+}
