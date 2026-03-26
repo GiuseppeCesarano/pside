b6f307014ff0bc96749e9c1699f98839fddc7ba1
Giuseppe Cesarano <PecoraInPannaCotta@gmail.com>
2026-03-26 11:41:21 +0100

    Debug info works with executable elf type

--- src/userspace/report/DebugInfo.zig
+++ src/userspace/report/DebugInfo.zig
@@ -8,8 +8,8 @@ pub const Location = union(enum) {
     },
     ip: u64,
 
-    pub fn deinit(this: Location, allocator: std.mem.Allocator) void {
-        switch (this) {
+    pub fn deinit(self: Location, allocator: std.mem.Allocator) void {
+        switch (self) {
             .resolved => |r| {
                 if (r.function) |f| allocator.free(f);
                 if (r.file) |f| allocator.free(f);
@@ -20,89 +20,89 @@ pub const Location = union(enum) {
 };
 
 const DebugInfo = @This();
+
 dwarf: ?std.debug.Dwarf,
 mmap: ?std.Io.File.MemoryMap,
 text_vaddr: u64,
 endian: std.builtin.Endian,
 
+const empty: DebugInfo = .{
+    .dwarf = null,
+    .mmap = null,
+    .text_vaddr = 0,
+    .endian = .little,
+};
+
 pub fn load(allocator: std.mem.Allocator, io: std.Io, binary_path: []const u8) !DebugInfo {
-    const file = std.Io.Dir.cwd().openFile(io, binary_path, .{}) catch
-        return .{ .dwarf = null, .mmap = null, .text_vaddr = 0, .endian = .little };
+    const file = std.Io.Dir.cwd().openFile(io, binary_path, .{}) catch return empty;
     defer file.close(io);
 
+    const file_len: usize = @intCast(try file.length(io));
+    if (file_len < 64) return empty;
+
     var mmap = std.Io.File.MemoryMap.create(io, file, .{
-        .len = @intCast(try file.length(io)),
+        .len = file_len,
         .protection = .{ .read = true, .write = false },
         .populate = false,
-    }) catch return .{ .dwarf = null, .mmap = null, .text_vaddr = 0, .endian = .little };
+    }) catch return empty;
     errdefer mmap.destroy(io);
 
     const bytes = mmap.memory;
-    if (bytes.len < 64) return .{ .dwarf = null, .mmap = mmap, .text_vaddr = 0, .endian = .little };
 
     var reader: std.Io.Reader = .fixed(bytes);
-    const hdr = std.elf.Header.read(&reader) catch
-        return .{ .dwarf = null, .mmap = mmap, .text_vaddr = 0, .endian = .little };
+    const hdr = std.elf.Header.read(&reader) catch return .{ .dwarf = null, .mmap = mmap, .text_vaddr = 0, .endian = .little };
     const endian = hdr.endian;
 
-    var text_vaddr: u64 = 0;
-    var ph_iter = hdr.iterateProgramHeadersBuffer(bytes);
-    while (try ph_iter.next()) |ph| {
-        if (ph.p_type == std.elf.PT_LOAD and ph.p_flags & std.elf.PF_X != 0) {
-            text_vaddr = ph.p_vaddr;
-            break;
-        }
-    }
+    var ph_it = hdr.iterateProgramHeadersBuffer(bytes);
+    const text_vaddr = while (try ph_it.next()) |ph| {
+        if (ph.p_type == std.elf.PT_LOAD and ph.p_flags & std.elf.PF_X != 0) break ph.p_vaddr;
+    } else 0;
 
-    const shstrndx_sh = blk: {
-        var it = hdr.iterateSectionHeadersBuffer(bytes);
-        var i: usize = 0;
-        while (try it.next()) |sh| : (i += 1)
-            if (i == hdr.shstrndx) break :blk sh;
-        return .{ .dwarf = null, .mmap = mmap, .text_vaddr = text_vaddr, .endian = endian };
-    };
-    const shstrtab = bytes[shstrndx_sh.sh_offset..][0..shstrndx_sh.sh_size];
+    const bail: DebugInfo = .{ .dwarf = null, .mmap = mmap, .text_vaddr = text_vaddr, .endian = endian };
+
+    var sh_it = hdr.iterateSectionHeadersBuffer(bytes);
+    var i: usize = 0;
+    const shstrtab = while (try sh_it.next()) |sh| : (i += 1) {
+        if (i == hdr.shstrndx) break bytes[sh.sh_offset..][0..sh.sh_size];
+    } else return bail;
 
     var dwarf: std.debug.Dwarf = .{ .sections = @splat(null) };
-    var sh_iter = hdr.iterateSectionHeadersBuffer(bytes);
-    while (try sh_iter.next()) |sh| {
+    sh_it = hdr.iterateSectionHeadersBuffer(bytes);
+    while (try sh_it.next()) |sh| {
         if (sh.sh_size == 0) continue;
+
         const name = std.mem.sliceTo(shstrtab[sh.sh_name..], 0);
         const data = bytes[sh.sh_offset..][0..sh.sh_size];
-        const section: std.debug.Dwarf.Section = .{ .data = data, .owned = false };
+
         inline for (std.meta.fields(std.debug.Dwarf.Section.Id)) |field| {
-            const dot_name = "." ++ field.name;
-            if (std.mem.eql(u8, name, dot_name)) {
-                dwarf.sections[@intFromEnum(@field(std.debug.Dwarf.Section.Id, field.name))] = section;
+            if (std.mem.eql(u8, name, "." ++ field.name)) {
+                const section = @intFromEnum(@field(std.debug.Dwarf.Section.Id, field.name));
+                dwarf.sections[section] = .{ .data = data, .owned = false };
             }
         }
     }
 
-    std.debug.Dwarf.open(&dwarf, allocator, endian) catch
-        return .{ .dwarf = null, .mmap = mmap, .text_vaddr = text_vaddr, .endian = endian };
+    std.debug.Dwarf.open(&dwarf, allocator, endian) catch return bail;
 
     return .{ .dwarf = dwarf, .mmap = mmap, .text_vaddr = text_vaddr, .endian = endian };
 }
 
-pub fn deinit(this: *DebugInfo, allocator: std.mem.Allocator) void {
-    if (this.dwarf) |*d| d.deinit(allocator);
-    if (this.elf_bytes.len > 0) std.posix.munmap(this.elf_bytes);
+pub fn deinit(self: *DebugInfo, allocator: std.mem.Allocator, io: std.Io) void {
+    if (self.dwarf) |*d| d.deinit(allocator);
+    if (self.mmap) |*m| m.destroy(io);
 }
 
-pub fn resolve(this: *DebugInfo, allocator: std.mem.Allocator, relative_ip: u64) !Location {
-    var dwarf = &(this.dwarf orelse return .{ .ip = relative_ip });
-    const addr = relative_ip + this.text_vaddr;
+pub fn resolve(self: *DebugInfo, allocator: std.mem.Allocator, relative_ip: u64) !Location {
+    const dwarf = &(self.dwarf orelse return .{ .ip = relative_ip });
+    const addr = relative_ip + self.text_vaddr;
 
-    const symbol = dwarf.getSymbol(allocator, this.endian, addr) catch
+    const symbol = dwarf.getSymbol(allocator, self.endian, addr) catch
         return .{ .ip = relative_ip };
 
     const src = symbol.source_location;
-
-    return .{
-        .resolved = .{
-            .function = if (symbol.name) |n| try allocator.dupe(u8, n) else null,
-            .file = if (src) |s| s.file_name else null,
-            .line = if (src) |s| @intCast(s.line) else 0,
-        },
-    };
+    return .{ .resolved = .{
+        .function = if (symbol.name) |n| try allocator.dupe(u8, n) else null,
+        .file = if (src) |s| s.file_name else null,
+        .line = if (src) |s| @intCast(s.line) else 0,
+    } };
 }
